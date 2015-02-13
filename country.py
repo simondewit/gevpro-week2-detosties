689e851c9a6824c3fe1536c207b41d5c0bf02f1d
Simon de Wit <simon@RN-145-97-136-149.eduroam.rug.nl>
2015-02-13 10:23:12 +0100

    Added docstrings

--- country.py
+++ country.py
@@ -1,13 +1,22 @@
-#!/usr/bin/env python
+#!/opt/local/bin/python3.4
 # Simon de Wit, januari 2015
 
 
 class Country():
     def __init__(self, name):
+        """
+        Creates a Country object with 'name' as name.
+        """
         self.name = name
 
     def getName(self):
+        """
+        Returns the name of the Country object.
+        """
         return self.name
 
     def __str__(self):
+        return "{}{}".format("Hello from ", self.name)
+
+    def __repr__(self):
         return "{}{}".format("Hello from ", self.name)
