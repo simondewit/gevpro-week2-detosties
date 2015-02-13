cb42c72dde3bbd32fb25c4811162cc4d99abdcac
Simon de Wit <simon@RN-145-97-136-149.eduroam.rug.nl>
2015-02-13 10:55:19 +0100

    Worked some more

--- flag_ui.py
+++ flag_ui.py
@@ -1,2 +1,28 @@
 #!/opt/local/bin/python3.4
-# Simon de Wit, januari 2015
+# Simon de Wit, februari 2015
+
+import sys
+from country import Country
+from PyQt4 import QtCore, QtGui
+
+
+class Flags(QtGui.QWidget):
+    def __init__(self):
+        super(Flags, self).__init__()
+        self.initUI()
+
+    def iniUI(self):
+        self.setWindowTitle("Country Flags")
+
+        # Set grid layout
+        grid = QtGui.QGridLayout()
+        self.setLayout(grid)
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    f = Flags()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
