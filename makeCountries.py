ff233fcd379ce00449f36aca9d61483925d1697f
Simon de Wit <simon@MacBook-Pro-van-Simon.local>
2015-02-15 21:19:29 +0100

    Modified function

--- makeCountries.py
+++ makeCountries.py
@@ -1,13 +1,13 @@
 from country import Country
 
 
-def makeCountries(fname, l):
+def makeCountries():
     """
     Makes 2 lists, 1 with names that *.txt contains, and 1 with Country-objects with the names of list 1.
     """
     countries = []
     countries_obj = []
-    with open('countries_list.txt', 'r') as in_f:
+    with open("countries_list.txt") as in_f:
         for line in in_f:
             countries.append(line.strip())
             countries_obj.append(Country(line.strip()))
