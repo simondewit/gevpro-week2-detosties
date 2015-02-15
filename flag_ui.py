f383027318e4b18e98a07ee5d005e6949ef54247
Simon de Wit <simon@MacBook-Pro-van-Simon.local>
2015-02-15 21:20:39 +0100

    Modified

--- flag_ui.py
+++ flag_ui.py
@@ -2,6 +2,8 @@
 # Simon de Wit, februari 2015
 
 import sys
+from makeCountries import *
+from flag_color import *
 from country import Country
 from PyQt4 import QtCore, QtGui
 
@@ -12,11 +14,33 @@ class Flags(QtGui.QWidget):
         self.initUI()
 
     def iniUI(self):
+
+        flags = {}
+
+        self.c_list, c_obj_list = makeCountries()
+
+        for i in self.c_list:
+            flags[i] = FlagColor()
+
         self.setWindowTitle("Country Flags")
 
         # Set grid layout
         grid = QtGui.QGridLayout()
         self.setLayout(grid)
+
+        self.c1 = QtGui.QComboBox(self)
+        self.c1.addItems(self.c_list)
+        grid.addWidget(self.c1, 1, 0)
+        self.c1.setCurrentIndex(13)
+        self.c1.currentIndexChanged.connect(self.updateUI)
+
+    def updateUI(self):
+        """
+        Updates the flag.
+        """
+        flags[self.c1.currentText()] =
+
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
