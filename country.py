9fd630ac38128d7d7fa91e410b842a54116632ff
Simon de Wit <simon@MacBook-Pro-van-Simon.local>
2015-02-17 10:35:41 +0100

    Added color name / flag attribute

--- country.py
+++ country.py
@@ -3,17 +3,24 @@
 
 
 class Country():
-    def __init__(self, name):
+    def __init__(self, name, colour):
         """
         Creates a Country object with 'name' as name.
         """
         self.name = name
+        self.colour = colour
 
     def getName(self):
         """
         Returns the name of the Country object.
         """
         return self.name
+
+    def getColour(self):
+        """
+        Returns the color name of the Country object.
+        """
+        return self.colour
 
     def __str__(self):
         return "{}{}".format("Hello from ", self.name)
